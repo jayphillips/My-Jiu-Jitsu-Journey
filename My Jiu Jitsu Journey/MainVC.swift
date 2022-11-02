ed47ec278a1bb6db592bdfea2f81e775e4d3467a
Jay Phillips <jayphillips@icloud.com>
2022-11-02 18:37:07 -0400

    Initial Commit

---
+++ My Jiu Jitsu Journey/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  MainVC.swift
+//  My Jiu Jitsu Journey
+//
+//  Created by Jay Phillips on 10/30/22.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
